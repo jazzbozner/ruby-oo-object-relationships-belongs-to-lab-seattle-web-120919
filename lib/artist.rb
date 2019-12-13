2c1ed8710deff5be1193208a9589a1b1f7637afd
github username <github email address>
2019-12-13 12:00:10 -0800

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,14 @@
+require 'pry'
+class Artist
+
+    attr_accessor :name
+
+    # def name=(name)
+    #     @name = name
+    # end
+
+    # def name
+    #     @name
+    # end
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Post
+
+    attr_accessor :title, :author
+    # attr_access
+    # def title=(title)
+    #     @title = title
+    # end
+
+    # def title
+    #     @title
+    # end
+
+end
